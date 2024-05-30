7b0667293612e1d8064e4b24b558bedac5cbc5ba
Mateusz Różański <rozanski.mateusz00@gmail.com>
2024-05-30 02:37:43 +0200

    feature: add oauth request method enumeration

---
+++ src/LeanOAuth.Core/OAuthRequestMethod.cs
@@ -0,0 +1,11 @@
+namespace LeanOAuth.Core;
+
+public sealed class OAuthRequestMethod : Enumeration<OAuthRequestMethod>
+{
+    public static readonly OAuthRequestMethod Get = new(1, "GET");
+    public static readonly OAuthRequestMethod Post = new(2, "POST");
+
+    private OAuthRequestMethod(int value, string name) : base(value, name)
+    {
+    }
+}
