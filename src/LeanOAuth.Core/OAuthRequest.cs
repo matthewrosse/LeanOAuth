9a7df63d3fea3a90fad4685cdd8933bedfe3d516
Mateusz Różański <rozanski.mateusz00@gmail.com>
2024-06-05 01:30:45 +0200

    feat(OAuthRequests): add a base class for all types of OAuth1.0A requests

---
+++ src/LeanOAuth.Core/OAuthRequest.cs
@@ -0,0 +1,19 @@
+namespace LeanOAuth.Core;
+
+public abstract class OAuthRequest
+{
+    public required OAuthRequestMethod RequestMethod { get; init; }
+    public required string ConsumerKey { get; init; }
+    public required OAuthSignatureMethod SignatureMethod { get; init; }
+    public required long Timestamp { get; init; }
+    public required string Nonce { get; init; }
+
+    public string Version => "1.0";
+
+    public IDictionary<string, string> AdditionalParameters { get; init; } =
+        new Dictionary<string, string>();
+
+    public abstract string GetAuthorizationHeader();
+
+    public abstract string GetAuthorizationQuery();
+}
