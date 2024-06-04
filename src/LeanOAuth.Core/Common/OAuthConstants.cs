b90e55417fc9864e293e598610aaee3cb37e9ad5
Mateusz Różański <rozanski.mateusz00@gmail.com>
2024-06-05 01:48:37 +0200

    feat: add OAuthConstants static class with common parameter names to reduce magic strings in the code

---
+++ src/LeanOAuth.Core/Common/OAuthConstants.cs
@@ -0,0 +1,20 @@
+namespace LeanOAuth.Core.Common;
+
+public static class OAuthConstants
+{
+    public const string AuthorizationHeader = "Authorization";
+
+    public static class Parameters
+    {
+        public const string ConsumerKey = "oauth_consumer_key";
+        public const string Nonce = "oauth_nonce";
+        public const string Timestamp = "oauth_timestamp";
+        public const string SignatureMethod = "oauth_signature_method";
+        public const string Signature = "oauth_signature_method";
+        public const string Version = "oauth_version";
+        public const string Callback = "oauth_callback";
+        public const string Token = "oauth_token";
+        public const string TokenSecret = "oauth_token_secret";
+        public const string Verifier = "oauth_verifier";
+    }
+}
