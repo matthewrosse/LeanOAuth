9db058eeecaaa68e846cea7bb4cf5dc7281266cd
Mateusz Różański <rozanski.mateusz00@gmail.com>
2024-06-08 02:57:49 +0200

    fix: fix bug with signature parameter name value

--- src/LeanOAuth.Core/Common/OAuthConstants.cs
+++ src/LeanOAuth.Core/Common/OAuthConstants.cs
@@ -11,7 +11,7 @@ public static class OAuthConstants
         public const string Nonce = "oauth_nonce";
         public const string Timestamp = "oauth_timestamp";
         public const string SignatureMethod = "oauth_signature_method";
-        public const string Signature = "oauth_signature_method";
+        public const string Signature = "oauth_signature";
         public const string Version = "oauth_version";
         public const string Callback = "oauth_callback";
         public const string Token = "oauth_token";
