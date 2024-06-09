fe43326601ee694d077dfd64c0f8e9018c242fef
Mateusz Różański <rozanski.mateusz00@gmail.com>
2024-06-10 00:10:00 +0200

    refactor: delete json property name settings from UnauthorizedRequestTokenResponse class, since the response type is application/x-www-form-urlencoded, not a JSON

--- src/LeanOAuth.Core/Responses/UnauthorizedRequestTokenResponse.cs
+++ src/LeanOAuth.Core/Responses/UnauthorizedRequestTokenResponse.cs
@@ -1,15 +1,7 @@
-using System.Text.Json.Serialization;
-using LeanOAuth.Core.Common;
-
 namespace LeanOAuth.Core.Responses;
 
 public record UnauthorizedRequestTokenResponse(
-    [property: JsonPropertyName(OAuthConstants.Responses.UnauthorizedRequestToken.Token)]
-        string Token,
-    [property: JsonPropertyName(OAuthConstants.Responses.UnauthorizedRequestToken.TokenSecret)]
-        string TokenSecret,
-    [property: JsonPropertyName(
-        OAuthConstants.Responses.UnauthorizedRequestToken.CallbackConfirmed
-    )]
-        bool CallbackConfirmed
+    string Token,
+    string TokenSecret,
+    bool CallbackConfirmed
 );
