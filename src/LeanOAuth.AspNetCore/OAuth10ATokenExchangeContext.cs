be263bdd470c9cdd2fd3bd3a7ed29f15bc4d9cc5
Mateusz Różański <rozanski.mateusz00@gmail.com>
2024-06-11 01:37:56 +0200

    feat: add OAuth10ATokenExchangeContext

---
+++ src/LeanOAuth.AspNetCore/OAuth10ATokenExchangeContext.cs
@@ -0,0 +1,9 @@
+using Microsoft.AspNetCore.Authentication;
+
+namespace LeanOAuth.AspNetCore;
+
+public record OAuth10ATokenExchangeContext(
+    AuthenticationProperties Properties,
+    string Token,
+    string TokenSecret
+);
