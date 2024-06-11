e2d130972e3519055b8fdf3ce5b60e3fb430555d
Mateusz Różański <rozanski.mateusz00@gmail.com>
2024-06-12 01:00:52 +0200

    docs: add OAuth10ATokenExchangeContext.cs docs

--- src/LeanOAuth.AspNetCore/Events/OAuth10ATokenExchangeContext.cs
+++ src/LeanOAuth.AspNetCore/Events/OAuth10ATokenExchangeContext.cs
@@ -2,6 +2,13 @@ using Microsoft.AspNetCore.Authentication;
 
 namespace LeanOAuth.AspNetCore.Events;
 
+/// <summary>
+/// Contains information used to perform the access token exchange.
+/// </summary>
+/// <param name="Properties">The <see cref="AuthenticationProperties"/>.</param>
+/// <param name="Token">The access token.</param>
+/// <param name="TokenSecret">The access token secret.</param>
+/// <param name="Verifier">The code verifier.</param>
 public record OAuth10ATokenExchangeContext(
     AuthenticationProperties Properties,
     string Token,
