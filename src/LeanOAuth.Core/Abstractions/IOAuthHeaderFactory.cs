456a314b12e70b3a5eb70e75e039b6f4518b48a0
Mateusz Różański <rozanski.mateusz00@gmail.com>
2024-06-11 17:41:41 +0200

    feat: add access token related methods to the auth header factory interface

--- src/LeanOAuth.Core/Abstractions/IOAuthHeaderFactory.cs
+++ src/LeanOAuth.Core/Abstractions/IOAuthHeaderFactory.cs
@@ -5,11 +5,26 @@ namespace LeanOAuth.Core.Abstractions;
 public interface IOAuthHeaderFactory<TOAuthOptions>
     where TOAuthOptions : IOAuthOptions
 {
-    string CreateRequestTokenHeader(HttpMethod httpMethod, Uri callbackUrl);
+    string CreateRequestTokenRequestHeader(HttpMethod httpMethod, Uri callbackUrl);
 
-    string CreateRequestTokenHeader(
+    string CreateRequestTokenRequestHeader(
         HttpMethod httpMethod,
         Uri callbackUrl,
+        IDictionary<string, string> additionalParameters
+    );
+
+    string CreateAccessTokenRequestHeader(
+        HttpMethod httpMethod,
+        string token,
+        string tokenSecret,
+        string verifier
+    );
+
+    string CreateAccessTokenRequestHeader(
+        HttpMethod httpMethod,
+        string token,
+        string tokenSecret,
+        string verifier,
         IDictionary<string, string> additionalParameters
     );
 }
